67319bf9ad349cfd1b66f992d789ccda90d1bb22
Tom Hynes <thomas.hynes@outlook.com>
2017-09-27 16:53:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function theBeatlesPlay(musicians,instruments) {
+<<<<<<< HEAD
   var array=[]
   for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + " plays " + instruments[i])
@@ -23,4 +24,11 @@ do {
  number++
 } while (number < 15)
  return array
+=======
+  var i
+  for (i = 0; i < 4; i++) {
+    theBeatlesPlay.push(musicians[i] + " plays " + instruments[i])
+  }
+  return theBeatlesPlay
+>>>>>>> b2bf6c156ca52d3872d52670a44b1739f059d565
 }
